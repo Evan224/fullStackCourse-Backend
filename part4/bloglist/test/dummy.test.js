aaf669fac2dadeb447f3ee976d393f9397709d50
Evan <2248392717@qq.com>
2022-09-13 00:12:51 +0300

    4.4: helper functions and unit tests, step2

--- part4/bloglist/test/dummy.test.js
+++ part4/bloglist/test/dummy.test.js
@@ -6,3 +6,21 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
   })
+
+  describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a6',
+        title: 'Not titled',
+        author: 'HSAIODH',
+        url: 'http://www.ations/Go_To_Considered_Harmful.html',
+        likes: 2,
+        __v: 0
+      }
+    ]
+  
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(2)
+    })
+  })
